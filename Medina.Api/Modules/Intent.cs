b1e01416716325ddaa678fdf021f4a0b162bdcfe
six <six@rch.dev>
2019-03-14 20:28:12 -0400

    basic nancy route

---
+++ Medina.Api/Modules/Intent.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+
+namespace Medina.Api.Modules
+{
+    public class Intent : NancyModule
+    {
+        public Intent()
+        {
+            After.AddItemToEndOfPipeline((ctx) =>
+            {
+                ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "POST,GET")
+                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+            });
+
+            Get["/"] = parameters =>
+            {
+                return "handshake successful";
+            };
+        }
+    }
+}
